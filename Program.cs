bfd8f8af92a9df97c4e6b0491a7e9263ccb6ab1c
Your Name <you@example.com>
2022-02-16 10:15:42 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homework15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Hello this is my homework" };
+
+            foreach (string join in array)
+            {
+                foreach (char text in join)
+                {
+                    if (char.IsUpper(text))
+                    {
+                        Console.Write(char.ToLower(text));
+                    }
+                    else
+                    {
+                        Console.Write(char.ToUpper(text));
+                    }
+                    
+
+                }
+            }
+        }
+      
+        
+
+         
+     
+    }
+}
